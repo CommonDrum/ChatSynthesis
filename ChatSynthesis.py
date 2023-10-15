a60018382d695616a855d0fb43e44d800e1ad156
CommonDrum <karol.hetman@protonmail.com>
2023-10-16 00:48:19 +0100

    Commandile use better

--- ChatSynthesis.py
+++ ChatSynthesis.py
@@ -1,37 +1,67 @@
-import argparse
-import os
+import sys
 from application import Application
+import openai
+import platform
+import os
 
-parser = argparse.ArgumentParser(description='Set API keys or run the application')
-parser.add_argument('--api_key_1', type=str, help='API key 1')
-parser.add_argument('--api_key_2', type=str, help='API key 2')
-parser.add_argument('--api_key_3', type=str, help='API key 3')
-parser.add_argument('--prompt', type=str, help='Prompt to generate chat response')
-args = parser.parse_args()
 
-if args.api_key_1 and args.api_key_2 and args.api_key_3:
-    os.environ['API_KEY_1'] = args.api_key_1
-    os.environ['API_KEY_2'] = args.api_key_2
-    os.environ['API_KEY_3'] = args.api_key_3
-else:
-    if args.prompt:
-        app = Application(prompt=args.prompt)
+def save_persistent_variable(name, value):
+    os_type = platform.system()
+
+    if os_type == "Windows":
+        import winreg as reg
+        key_path = r'SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
+        try:
+            with reg.OpenKey(reg.HKEY_LOCAL_MACHINE, key_path, 0, reg.KEY_SET_VALUE | reg.KEY_WOW64_64KEY) as env_key:
+                reg.SetValueEx(env_key, name, 0, reg.REG_SZ, value)
+                print(f"{name}={value} added to system environment variables")
+        except PermissionError:
+            print("Administrator privileges are required to set system environment variables on Windows.")
+        
+    elif os_type in ["Linux", "Darwin"]:
+        profile_path = os.path.expanduser("~/.profile")
+        with open(profile_path, "a") as f:
+            f.write(f"\nexport {name}={value}\n")
+            print(f"{name}={value} added to {profile_path}")
+
     else:
-        app = Application()
-    app.run()
+        print(f"Unsupported OS type: {os_type}")
+
+
+def set_variable_if_not_exists(name):
+    existing_value = os.environ.get(name)
+
+    if existing_value is None:
+        new_value = input(f"Environment variable {name} not found. Please enter its value: ")
+        
+        save_persistent_variable(name, new_value)
+    else:
+        print(f"Environment variable {name} already exists with value: {existing_value}")
+
+
+
+
+
 import os
-from application import Application
 
-parser = argparse.ArgumentParser(description='Set API keys or run the application')
-parser.add_argument('--api_key_1', type=str, help='API key 1')
-parser.add_argument('--api_key_2', type=str, help='API key 2')
-parser.add_argument('--api_key_3', type=str, help='API key 3')
-args = parser.parse_args()
+if len(sys.argv) > 1:
+    if "OPENAI_API_KEY" not in os.environ:
+        raise Exception("OPENAI_API_KEY environment variable not set")
+    if "AWS_ACCESS_KEY" not in os.environ:
+        raise Exception("AWS_ACCESS_KEY environment variable not set")
+    if "AWS_SECRET_KEY" not in os.environ:
+        raise Exception("AWS_SECRET_KEY environment variable not set")
+        
+    
+    openai.api_key = os.environ["OPENAI_API_KEY"]
 
-if args.api_key_1 and args.api_key_2 and args.api_key_3:
-    os.environ['API_KEY_1'] = args.api_key_1
-    os.environ['API_KEY_2'] = args.api_key_2
-    os.environ['API_KEY_3'] = args.api_key_3
+    prompt = " ".join(sys.argv[1:])
+    #app = Application()
+    #app.run(prompt) #TODO: this is too much code change it later
 else:
-    app = Application()
-    app.run()
+    set_variable_if_not_exists("OPENAI_API_KEY")
+    set_variable_if_not_exists("AWS_ACCESS_KEY")
+    set_variable_if_not_exists("AWS_SECRET_KEY") 
+
+    print("All keys ready. Use ./ChatSynthesis.py <prompt> to generate text for speech synthesis.")
+
--- application.py
+++ application.py
@@ -10,59 +10,17 @@ class Application:
 
     prompt_adjustment = "Generate text for speech synthesis for the following text:\n "
 
-    
-
-
-
     def __init__(self):
-
-       
-        try:
-            with open('config.json') as json_file:
-                data = json.load(json_file)
-                self.api_key = data['openai_api_key']
-                self.access_key = data['aws_access_key']
-                self.secret_key = data['aws_secret_key']
-        except:
-            print("Provide data for authentication: (Your data will be saved in config.json file)\n")
-            self.api_key = input("OpenAI API key: ")
-            self.access_key = input("AWS access key: ")
-            self.secret_key = input("AWS secret key: ")
-            data = {}
-            data['openai_api_key'] = self.api_key
-            data['aws_access_key'] = self.access_key
-            data['aws_secret_key'] = self.secret_key
-            with open('config.json', 'w') as outfile:
-                json.dump(data, outfile)
-
-        openai.api_key = self.api_key
-
-
-    
-
-
-        #self.chat = Chat(api_key)
         self.synthezator = synthezator(self.access_key, self.secret_key)
 
-    def run(self):
-        print("Hello, I'm a chatbot. Ask me anything!\n")
-        while True:
-            question = input("You: ")
-
-            if question == "exit":
-                break
-
-            question = self.prompt_adjustment + question
-
-            anwser = self.comp(question)
-
-            self.synthezator.synthesize(anwser, "output.mp3")
-
-
-            file = "output.mp3"
-            os.system("afplay " + file)
-
-            print("Bot: " + anwser)
+    def run(self, prompt):
+        
+        prompt = self.prompt_adjustment + prompt
+        anwser = Chat.comp(prompt)
+        self.synthezator.synthesize(anwser)
+        file = "output.mp3"
+        os.system("afplay " + file)
+        print("Bot: " + anwser)
 
 
     def comp(PROMPT, MaxToken=3000, outputs=1): 
